0b3390538ce4c6f9c95ef94d2a9e90b72b1fc9d8
Jose <okwuosaifechidelu@gmail.com>
2025-01-29 11:24:43 +0100

    fixed layout padding

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -7,14 +7,20 @@ const Home = async () => {
     try {
         return (
             <div className='flex-1 grid grid-cols-2 xs:grid-cols-1 w-full xs:grid-sizes'>
-                <div className='w-[454px] xs:w-full mx-auto my-auto'>
-                    <div className='typo-heading_small_bold text-primary fixed top-[47px] flex w-full xs:inset-0 xs:mt-[46px] xs:justify-center'>
+                <div className='w-[454px] xs:w-full mx-auto'>
+                    <div className='typo-heading_small_bold text-primary flex w-full mt-[46px] xs:justify-center'>
                         Flipit
                     </div>
                     <Form />
                 </div>
-                <div className='bg-primary text-white flex items-center justify-center flex-col xs:hidden'>
-                    <Image src={'/auth-banner.png'} height={365} width={365} alt='banner' />
+                <div className='h-full bg-primary text-white flex items-center justify-center flex-col xs:hidden'>
+                    <Image
+                        src={'/auth-banner.png'}
+                        height={365}
+                        width={365}
+                        alt='banner'
+                        className='h-[365px] w-[365px]'
+                    />
                     <h2 className='typo-heading_large_bold mt-[31px]'>Trade smart, swap easy</h2>
                     <p className='w-[492px] typo-heading_small_regular text-center  mt-2'>
                         Turn the items you have into the ones you need. Start trading today!
