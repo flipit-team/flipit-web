fd38ffeb4d5de952cc32be171cf1d250eff5dabc
Jose <okwuosaifechidelu@gmail.com>
2025-02-22 21:50:49 +0100

    added route

--- app/ui/common/buttons/auth-button/index.tsx
+++ app/ui/common/buttons/auth-button/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 interface Props {
@@ -10,12 +11,13 @@ interface Props {
 const AuthButton = (props: Props) => {
     const {bg, title, icon, border} = props;
     return (
-        <div
+        <Link
+            href={'/home'}
             className={`w-full h-[45px] ${bg ? 'bg-primary text-white' : 'text-text_four'} ${border ? 'border border-border_gray' : ''} flex items-center px-4 rounded-[43px] typo-body_medium_semibold cursor-pointer`}
         >
             {icon ? <Image alt='icon' height={24} width={24} src={icon} className='h-6 w-6' /> : <></>}
             <p className='mx-auto'>{title}</p>
-        </div>
+        </Link>
     );
 };
 
