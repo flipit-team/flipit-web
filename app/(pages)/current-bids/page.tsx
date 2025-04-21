17bdd47494f3d5da151a239443c96058459e6681
Jose <okwuosaifechidelu@gmail.com>
2025-04-21 19:09:56 +0100

    again

--- app/(pages)/current-bids/page.tsx
+++ app/(pages)/current-bids/page.tsx
@@ -1,12 +1,7 @@
-import {Suspense} from 'react';
 import CurrentBids from '~/ui/wrappers/CurrentBids';
 
 const page = () => {
-    return (
-        <Suspense fallback={<p>Loading...</p>}>
-            <CurrentBids />
-        </Suspense>
-    );
+    return <CurrentBids />;
 };
 
 export default page;
